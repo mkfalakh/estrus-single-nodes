955515832aba0b86c5116bfcedcd4037b1b4fd3f
mkfalakh <falakh7791@gmail.com>
2026-06-14 21:01:00 +0700

    Include webServerTask in watchdog logging
    Add a printTask call for "webServerTask" with webServerTaskHandle inside watchdogTask so the web server task is included in the task-monitor output alongside wifiTask and Cleanup. This change improves visibility of the web server task in the watchdog logging.

--- estrus_single/task_monitor.cpp
+++ estrus_single/task_monitor.cpp
@@ -69,6 +69,10 @@ void watchdogTask(void* pv) {
       "wifiTask",
       wifiTaskHandle);
 
+      printTask(
+      "webServerTask",
+      webServerTaskHandle);
+
     printTask(
       "Cleanup",
       cleanupStorageTaskHandle);
