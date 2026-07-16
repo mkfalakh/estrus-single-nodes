e00bcb0d68d50f0ef2b673074054e384cdabb8f6
mkfalakh <falakh7791@gmail.com>
2026-07-16 19:53:36 +0700

    Comment out wifiTask function
    Temporarily disable the WiFi idle timeout task. The function that monitored WiFi idle time and disabled the AP after a timeout is now commented out in both the implementation and header files.

--- estrus_single/wifi_task.cpp
+++ estrus_single/wifi_task.cpp
@@ -5,28 +5,28 @@
 
 #define WIFI_TIMEOUT_MS 600000UL  // 10 menit
 
-void wifiTask(void *pv) {
+// void wifiTask(void *pv) {
 
-  while (true) {
+//   while (true) {
 
-    if (wifiEnabled) {
+//     if (wifiEnabled) {
 
-      unsigned long idleTime =
-        millis() - lastClientTime;
+//       unsigned long idleTime =
+//         millis() - lastClientTime;
 
-      if (idleTime >= WIFI_TIMEOUT_MS) {
+//       if (idleTime >= WIFI_TIMEOUT_MS) {
 
-        logToFile(
-          "WiFi idle=%lu timeout=%lu enabled=%d",
-          idleTime,
-          WIFI_TIMEOUT_MS,
-          wifiEnabled);
+//         logToFile(
+//           "WiFi idle=%lu timeout=%lu enabled=%d",
+//           idleTime,
+//           WIFI_TIMEOUT_MS,
+//           wifiEnabled);
 
-        disableWiFiAP();
-      }
-    }
+//         disableWiFiAP();
+//       }
+//     }
 
-    vTaskDelay(
-      5000 / portTICK_PERIOD_MS);
-  }
-}
+//     vTaskDelay(
+//       5000 / portTICK_PERIOD_MS);
+//   }
+// }
--- estrus_single/wifi_task.h
+++ estrus_single/wifi_task.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void wifiTask(void *pv);
+// void wifiTask(void *pv);
